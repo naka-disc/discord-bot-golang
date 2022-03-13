f7e3540a042798baca5fe243b278b1c9c892fdf6
naka-disc <99717730+naka-disc@users.noreply.github.com>
2022-03-13 23:42:23 +0900

    Improve: ボイスチャンネル入退室時処理を別パッケージに分離

--- cmd/main.go
+++ cmd/main.go
@@ -45,7 +45,7 @@ func main() {
 
 	// イベントのコールバックを登録する
 	dg.AddHandler(service.MessageDispatch) // メッセージ受信時
-	dg.AddHandler(voiceStateUpdate)        // ボイスチャンネルへの入退室時
+	dg.AddHandler(service.VoiceDispatch)   // ボイスチャンネルへの入退室時
 	dg.AddHandler(guildMemberAdd)          // サーバーへの入室時
 
 	// Discordセッションの権限付与
@@ -67,52 +67,6 @@ func main() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-}
-
-// Dicordセッションに追加するコールバック関数。
-// voiceStateUpdate: VoiceChannelに入退室した時にキックされる処理。
-func voiceStateUpdate(session *discordgo.Session, voiceState *discordgo.VoiceStateUpdate) {
-	// 入退室のどちらかを取得
-	// 退室時のステートはChannelIDがブランクになってるのでそれで判断
-	isJoin := (voiceState.VoiceState.ChannelID != "")
-
-	// 入室か退室かで処理を分岐
-	if isJoin {
-		// 入室時の処理
-		fmt.Println("Join.")
-
-		// 入室時は退室日時と滞在時間以外を登録
-		// TODO: voiceStateにユーザー名とかがないので登録してない
-		// どこかしらでユーザー情報を取得して、DiscordMemberTableとか作ってリレーション張った方がいいかも
-		saveEntity := entity.NewVcAccessLog()
-		saveEntity.DiscordMemberId = voiceState.VoiceState.UserID
-		saveEntity.VoiceChannelId = voiceState.ChannelID
-		saveEntity.JoinDatetime = dateutil.GetNowString()
-
-		valDAO := dao.NewVcAccessLogDAO()
-		valDAO.AddVcAccessLog(saveEntity)
-
-	} else {
-		// 退室時の処理
-		fmt.Println("Leave.")
-
-		// 退室時は、直近の入室のみデータを引っ張ってきて、それに退室情報を追加して更新をかける
-		memberId := voiceState.VoiceState.UserID
-		channelId := voiceState.BeforeUpdate.ChannelID
-		valDAO := dao.NewVcAccessLogDAO()
-		getEntity, ok := valDAO.GetVcAccessLogForNotLeave(channelId, memberId)
-		if !ok {
-			// データ取得できなかったので終了
-			return
-		}
-
-		// 退室日時と滞在時間を更新
-		// 滞在時間を計算できなかった場合も更新 退室日時がわかっていればあとから計算して後付けが可能なため
-		leavetime := dateutil.GetNowString()
-		staySecond, _ := dateutil.DiffSecond(getEntity.JoinDatetime, leavetime)
-		overrideMap := map[string]interface{}{"leave_datetime": leavetime, "stay_second": staySecond}
-		valDAO.EditVcAccessLog(getEntity, overrideMap)
-	}
 }
 
 // Dicordセッションに追加するコールバック関数。
