ce8efe47c04a4025aac41c7f89ca38d2db26c5ce
naka-disc <99717730+naka-disc@users.noreply.github.com>
2022-03-13 23:39:57 +0900

    Fix: コメント追加

--- internal/app/service/Message.go
+++ internal/app/service/Message.go
@@ -2,6 +2,8 @@ package service
 
 import "github.com/bwmarrin/discordgo"
 
+// メッセージ受信時のディスパッチ処理。
+// 内容に応じて処理を分岐する。
 func MessageDispatch(s *discordgo.Session, mc *discordgo.MessageCreate) {
 	// DiscordセッションのユーザーID（=BotのID）と発言者のIDが一緒なら何もしない
 	if mc.Author.ID == s.State.User.ID {
